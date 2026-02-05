dc7e8756b82cc33fad1541e53a0205356a3348e9
ashaz-dewan <dewansklick@gmail.com>
2026-02-05 18:48:41 -0400

    Update EX03_variables_scoreboard_comments_only.py

--- week1/EX03_variables_scoreboard_comments_only.py
+++ week1/EX03_variables_scoreboard_comments_only.py
@@ -1,19 +1,4 @@
-# EX03 - Variables: Mini Scoreboard (COMMENTS ONLY)
-#
-# Goal:
-#   Create two variables:
-#     - player (a name as text)
-#     - score (a number)
-#
-# Then print TWO lines:
-#   Player: <name>
-#   Score: <number>
-#
-# Rules:
-#   - Use a variable for the name (not typed inside print).
-#   - Use a variable for the score.
-#
-# Hint:
-#   player = "Aisha"
-#   score = 10
-#   print("Player:", player)
+Player="Ashaz"
+Score=10
+print(Player)
+print(Score)
