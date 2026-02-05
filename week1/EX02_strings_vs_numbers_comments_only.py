317021c71e8c4870924c8053db53dab380688916
ashaz-dewan <dewansklick@gmail.com>
2026-02-05 18:33:25 -0400

    Update EX02_strings_vs_numbers_comments_only.py

--- week1/EX02_strings_vs_numbers_comments_only.py
+++ week1/EX02_strings_vs_numbers_comments_only.py
@@ -1,14 +1,2 @@
-# EX02 - Strings vs Numbers (COMMENTS ONLY)
-#
-# Goal:
-#   Make the program output:
-#     1) 12
-#     2) 34
-#
-# Rules:
-#   - Line 1 must be math (a number + a number).
-#   - Line 2 must be text gluing (a "string" + a "string").
-#
-# Hint:
-#   7 + 5 gives 12
-#   "3" + "4" gives "34"
+print(7+5)
+print("3"+"4")
