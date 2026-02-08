18d44cc47f2d1d723fb47cffc5f69bad6fda160d
ashaz-dewan <dewansklick@gmail.com>
2026-02-08 13:34:29 -0400

    Update EX01_comparisons_comments_only.py

--- week2/EX01_comparisons_comments_only.py
+++ week2/EX01_comparisons_comments_only.py
@@ -1,20 +1,5 @@
-# Filename: EX01_comparisons_comments_only.py
-# EX01 - Comparisons (COMMENTS ONLY)
-#
-# Goal:
-#   Write a program that:
-#     1) Asks the user for TWO numbers
-#     2) Prints answers to these questions:
-#         - Are they equal?
-#         - Is the first bigger?
-#         - Is the first smaller?
-#
-# Rules:
-#   - Use input() twice
-#   - Convert both inputs to int
-#   - Use print() for each answer
-#
-# Hint:
-#   n1 = int(input("Number 1: "))
-#   n2 = int(input("Number 2: "))
-#   print("Are they equal? ->", n1 == n2)
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+print("Are the numbers equal: ", number1==number2)
+print("First number bigger: ", number1>number2)
+print("First number smaller: ", number1<number2)
