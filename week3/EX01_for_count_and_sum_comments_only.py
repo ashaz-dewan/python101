c1c07d118860cc2ac6d3c3058aa496edef037e4c
mathandcodingacademy <mathandcodingacademy@gmail.com>
2026-02-14 12:39:35 -0400

    Create EX01_for_count_and_sum_comments_only.py

---
+++ week3/EX01_for_count_and_sum_comments_only.py
@@ -0,0 +1,22 @@
+# Filename: EX01_for_count_and_sum_comments_only.py
+# EX01 - Count and Sum (COMMENTS ONLY)
+#
+# Goal:
+#   Ask the user for an integer n
+#   Then:
+#     - Print numbers from 1 to n
+#     - Also print the sum of 1 to n at the end
+#
+# Example:
+#   n = 5
+#   Output:
+#     1
+#     2
+#     3
+#     4
+#     5
+#     Sum = 15
+#
+# Rules:
+#   - Use a for loop with range()
+#   - Use a running total variable
